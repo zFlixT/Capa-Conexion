53671bc7f3db97ad4484f2d77ebadf4920f74d0e
zFlixT <pt0109032023@unab.edu.sv>
2024-08-27 20:24:32 -0600

    Agregar archivos de proyecto.

---
+++ CapaConexion/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CapaConexion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Data Source=ASUS_TUF_JEREMY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+            conexion.Open();
+            MessageBox.Show("Conectado");
+            conexion.Close();
+            MessageBox.Show("Gracias, Conexion Finalizada");
+        }
+    }
+}

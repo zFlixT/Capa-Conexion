fb8752ab6cca891ab460c9533a73d515bbc4ad8e
zFlixT <pt0109032023@unab.edu.sv>
2024-08-27 20:34:49 -0600

    Realización de la guia IX : Generando un modelo y extrayendo la información.

--- CapaConexion/Form1.cs
+++ CapaConexion/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using CapaConexion.Modelos;
 
 namespace CapaConexion
 {
@@ -28,7 +29,8 @@ namespace CapaConexion
             //------------------------
 
             String selectFrom = "";
-            selectFrom = selectFrom + "SELECT [CompanyName] " + "\n";
+            selectFrom = selectFrom + "SELECT " + "\n";
+            selectFrom = selectFrom + "      [CompanyName] " + "\n";
             selectFrom = selectFrom + "      ,[ContactName] " + "\n";
             selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
             selectFrom = selectFrom + "      ,[Address] " + "\n";
@@ -45,9 +47,24 @@ namespace CapaConexion
             SqlCommand comando = new SqlCommand(selectFrom, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
+            List<customers> Customers = new List<customers>();
+
             while (reader.Read())
             {
-                var customerId = reader["CompanyName"];
+
+                customers customers = new customers();
+                customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
+                customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
+                customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
+                customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
+                customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
+                customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
+                customers.PostalCode = reader["PostalCode"] == DBNull.Value ? "" : (String)reader["PostalCode"];
+                customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
+                customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
+                customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+
+                Customers.Add(customers);
             }
 
             MessageBox.Show("Conexion cerrada");
